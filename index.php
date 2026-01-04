55a3c055df38b3491ed2ac413cfb7be6c8287c7e
Eles007 <lyos00@mail.ru>
2026-01-04 23:19:14 +0400

    Add practice 23.2 - 31.1 and add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Мой проект</title>
+</head>
+<body>
+
+<h1>Практика</h1>
+
+<ul>
+    <li><a href="01_basics/index.php">01.Основы</a></li>
+</ul>
+
+</body>
+</html>
