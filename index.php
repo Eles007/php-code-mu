25d7fbc2f5ed439d6392fab575fdac6deb0ab4ae
Eles007 <lyos00@mail.ru>
2026-01-05 19:32:50 +0400

    Add practice 52.6 - 67

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 
 <ul>
     <li><a href="01_basics/index.php">01.Основы</a></li>
+    <li><a href="02_arrays/index.php">02.Массивы</a></li>
+    <li><a href="03_conditions/index.php"></a>02.Условия</li>
 </ul>
 
 </body>
