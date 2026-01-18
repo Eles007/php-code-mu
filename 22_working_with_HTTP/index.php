42c891300534d908269aaab5ec04c6ab03d0ad84
Eles007 <lyos00@mail.ru>
2026-01-18 22:49:46 +0400

    Add practice 395 - 404

--- 22_working_with_HTTP/index.php
+++ 22_working_with_HTTP/index.php
@@ -1,1 +1,46 @@
 <?php
+
+header('X-Content-type: text/html');
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>22_working_with_HTTP</title>
+</head>
+<body>
+<a href="../index.php"><h1>Главная страница</h1></a>
+
+<form action="request.php" method="post">
+    Имя <input name="name" type="text" value="Иван">
+    <input type="submit">
+</form>
+
+Метод <?= $_SERVER['REQUEST_METHOD']; ?> <br>
+Accept <?= $_SERVER['HTTP_ACCEPT']; ?> <br>
+Accept-Language <?= $_SERVER['HTTP_ACCEPT_LANGUAGE']; ?> <br>
+<?php
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value<br>";
+}
+
+$arr = ['a', 'b', 'c'];
+
+if (isset($_GET['key'])) {
+    $key = $_GET['key'];
+
+    if (isset($arr[$key])) {
+        echo $arr[$key];
+    } else {
+        http_response_code(404);
+        echo 'Not Found';
+    }
+} else {
+    http_response_code(403);
+    echo 'Forbidden';
+}
+?>
+</body>
+</html>
---
+++ 22_working_with_HTTP/request.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Request</title>
+</head>
+<body>
+<a href="index.php"><h1>Назад</h1></a></body>
+
+
+Метод <?= $_SERVER['REQUEST_METHOD']; ?>
+</html>
